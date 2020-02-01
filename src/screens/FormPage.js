aba8cf24cf28c20de0315ec441b50fcda5aacc09
sehmim2 <sehmim@Sehmims-MacBook-Pro.local>
2020-01-31 23:48:25 -0500

    yes

--- src/screens/FormPage.js
+++ src/screens/FormPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 
 const sendData = (navigate) => {
@@ -8,17 +8,19 @@ const sendData = (navigate) => {
 
 const FormPage = ({ navigation }) => {
     const { navigate } = navigation
+    const [isSubmit, setIsSubmit] = React.useState(true)
+
     return (
         <View style={styles.container}>
-            <Text style={{ flex: 5 }}>
-
-            </Text>
-            <Button
-                style={{}}
+            <View style={{ flex: 5 }}>
+                <Text style={{ color: 'white' }} ></Text>
+            </View>
+            <TouchableOpacity
                 title="Submit"
                 onPress={() => sendData(navigate)}>
-            </Button>
-        </View>
+                <Text>Submit</Text>
+            </TouchableOpacity>
+        </View >
     )
 }
 
