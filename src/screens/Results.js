11736a7014a1396501bae1f129339002203d4dde
sehmim2 <sehmim@Sehmims-MacBook-Pro.local>
2020-02-01 03:35:02 -0500

    add flatlistview on results

--- src/screens/Results.js
+++ src/screens/Results.js
@@ -1,20 +1,79 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native'
+import Constants from 'expo-constants';
+import screensStyles from '../styles/ScreensStyles';
+import { MyCard } from '../components/MyCard';
+
+const BANKS = [
+    {
+        name: 'Poop',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+        confidence: '88',
+        quote: '230'
+    },
+    {
+        name: 'Poop',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+        confidence: '88',
+        quote: '230'
+    },
+    {
+        name: 'Poop',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+        confidence: '88',
+        quote: '230'
+    },
+    {
+        name: 'Poop',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+        confidence: '88',
+        quote: '230'
+    },
+]
 
 const Results = (props) => {
 
     const { navigate } = props.navigation
 
     return (
-        <View style={{ flex: 6 }}>
-            <Text style={{ flex: 5 }}>Results</Text>
-            <Button
-                title="Re-calculate"
-                onPress={() => { navigate("LandingPage") }}
-            ></Button>
-        </View>
+        <>
+            <View style={screensStyles.ScreenWrapper}>
+                {/* <View style={styles.statusBar} /> */}
+
+                <View style={screensStyles.ScreenMainPage}>
+
+                    <Text style={{ color: 'white', fontSize: 40, textAlign: 'center', padding: 20 }} >Results</Text>
+                    <View style={[screensStyles.ScreenButtom, { alignItems: 'center' }]}>
+                        <View style={{ flex: 1 }}>
+
+                            <SafeAreaView style={{ width: 400 }}>
+                                <FlatList
+                                    data={BANKS}
+                                    renderItem={({ item }) => <MyCard name={item.name} confidence={item.confidence} quote={item.quote} />}
+                                    keyExtractor={item => item.id}
+                                />
+                            </SafeAreaView>
+
+                        </View>
+                        <TouchableOpacity
+                            style={[screensStyles.Button, { backgroundColor: 'rgb(14,198,221)', marginBottom: 30, marginTop: 30 }]}
+                            title="Submit"
+                            onPress={() => { navigate("LandingPage") }}>
+                            <Text>Re-calculate</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+            </View>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    statusBar: {
+        height: Constants.statusBarHeight,
+    },
+});
 
 export default Results
 
