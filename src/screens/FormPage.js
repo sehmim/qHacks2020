9e801182135ed12054f45b834ac5bd1f55246521
sehmim2 <sehmim@Sehmims-MacBook-Pro.local>
2020-02-01 02:31:23 -0500

    poo

--- src/screens/FormPage.js
+++ src/screens/FormPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity, Picker } from 'react-native'
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity, Picker, KeyboardAvoidingView } from 'react-native'
 
 import Constants from 'expo-constants';
 import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from 'react-native-simple-radio-button';
@@ -13,7 +13,15 @@ let radio_props = [
 ];
 
 
-const sendData = (navigate) => {
+const sendData = (navigate, vehicleInfo, vehicleDistance, age, location, isUsed) => {
+    let inputData = {
+        vehicleInfo,
+        vehicleDistance,
+        age,
+        location,
+        isUsed: 1 ? "Used" : "New"
+    }
+    console.log("Sedning Data: ", inputData)
     navigate("Results")
 }
 
@@ -21,13 +29,13 @@ const FormPage = ({ navigation }) => {
     const { navigate } = navigation
     const [isSubmit, setIsSubmit] = React.useState(true)
 
-    const [carMake, setCarMake] = React.useState('')
-    const [year, setYear] = React.useState('')
-    const [model, setModel] = React.useState('')
-
     const [isUsed, setIsUsed] = React.useState('')
 
+    const [vehicleInfo, setVehicleInfo] = React.useState('')
+    const [vehicleDistance, setVehicleDistance] = React.useState('')
 
+    const [age, setAge] = React.useState('')
+    const [location, setLocation] = React.useState('')
 
 
     return (
@@ -36,45 +44,50 @@ const FormPage = ({ navigation }) => {
 
             <View style={{ padding: 15, margin: 10, borderWidth: 2, borderColor: 'white', borderBottomColor: 'black', borderRadius: 10 }}>
                 <Text style={{ color: 'white', fontSize: 40, textAlign: 'center', borderWidth: 2, borderBottomColor: 'white', borderRadius: 20 }} >Info</Text>
-                <Text style={{ color: 'white', paddingTop: 20 }}>Vehicle year: </Text>
+                <KeyboardAvoidingView>
+                    <Text style={{ color: 'white', paddingTop: 20 }}>Vehicle year, Make and Model: </Text>
+                    <TextInput
+                        style={{ height: 30, color: 'white', fontSize: 20 }}
+                        placeholder="2010"
+                        onChangeText={(text) => setVehicleInfo(text)}
+                        value={vehicleInfo}
+                    />
+                </KeyboardAvoidingView>
+                <Text style={{ color: 'white', paddingTop: 15 }}>Driver Age: </Text>
                 <TextInput
                     style={{ height: 30, color: 'white', fontSize: 20 }}
-                    placeholder="2010"
-                    onChangeText={(text) => setYear(text)}
-                    value={year}
+                    placeholder="69"
+                    onChangeText={(text) => setAge(text)}
+                    value={age}
                 />
-                <Text style={{ color: 'white', paddingTop: 15 }}>Vehicle make: </Text>
+                <Text style={{ color: 'white', paddingTop: 15 }}>City: </Text>
                 <TextInput
                     style={{ height: 30, color: 'white', fontSize: 20 }}
-                    placeholder="Honda"
-                    onChangeText={(text) => setCarMake(text)}
-                    value={carMake}
+                    placeholder="Toronto"
+                    onChangeText={(text) => setLocation(text)}
+                    value={location}
                 />
-                <Text style={{ color: 'white', paddingTop: 15 }}>Vehicle model: </Text>
-                <TextInput
-                    style={{ height: 30, color: 'white', fontSize: 20 }}
-                    placeholder="Civic"
-                    onChangeText={(text) => setCarMake(setModel)}
-                    value={model}
-                />
+                <KeyboardAvoidingView behavior="padding" enabled>
+
+                    <Text style={{ color: 'white', paddingTop: 15 }}>Driving Distance: </Text>
+                    <TextInput
+                        style={{ height: 30, color: 'white', fontSize: 20 }}
+                        placeholder="100,000km"
+                        onChangeText={(text) => setVehicleDistance(text)}
+                        value={vehicleDistance}
+                    />
+                </KeyboardAvoidingView>
                 <Text style={{ color: 'white', paddingTop: 15, paddingBottom: 10 }}>Vehicle condition when purchased: </Text>
                 <RadioForm
                     radio_props={radio_props}
                     initial={0}
                     onPress={(value) => { setIsUsed(value) }}
                 />
-                <Text style={{ color: 'white', paddingTop: 15 }}>Driving Distance: </Text>
-                <TextInput
-                    style={{ height: 30, color: 'white', fontSize: 20 }}
-                    placeholder="100,000km"
-                    onChangeText={(text) => setCarMake(setModel)}
-                    value={model}
-                />
                 <View style={{ alignItems: 'center' }}>
                     <TouchableOpacity
                         style={[screensStyles.Button, { backgroundColor: 'rgb(14,198,221)' }]}
                         title="Submit"
-                        onPress={() => sendData(navigate)}>
+                        onPress={() => sendData(navigate, vehicleInfo, vehicleDistance, age, location, isUsed)}>
                         <Text>Submit</Text>
                     </TouchableOpacity>
                 </View>
