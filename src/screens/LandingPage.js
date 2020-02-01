25e6ea323d605c8784da045d6350b948ba1b1fba
Ivan Lytovka <ivanlytovka@gmail.com>
2020-02-01 15:48:21 -0500

    Change Landing Page

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 import styles from '../styles/ScreensStyles';
+// import Svg, {Circle, Rect} from 'react-native-svg';
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import * as firebase from 'firebase';
 import { firebaseConfig } from '../../FirebaseConfig'
+
 
 const FIREBASE_CONFIG = {
     apiKey: firebaseConfig.API_KEY,
@@ -26,7 +28,8 @@ const LandingPage = (props) => {
     const [isLoading, setIsLoading] = React.useState(true)
     const { navigate } = props.navigation;
 
-    setTimeout(() => { setIsLoading(false); }, 4000);
+
+    setTimeout(() => { setIsLoading(false); }, 1000);
 
     return (
         <>
@@ -35,12 +38,14 @@ const LandingPage = (props) => {
             <View style={styles.ScreenWrapper}>
                 {
                     isLoading ?
-                        <Image source={require('../styles/car.svg')} style={{width: 50, height: 50}} />
+                        <></>
                         :
                         <>
+
+                            <Text style={{ position: "absolute", top: "15%", left: "37%" }}>Carshurance</Text>
+
+                            <Image source={require('../styles/car.png')} style={{ width: "80%", height: "25%", position: "absolute", top: "35%" }} />
                             <View style={styles.ScreenMainPage}>
-                                <View style={{}}>
-                                </View>
                             </View>
                             <View style={styles.ScreenButtom}>
                                 <TouchableOpacity style={styles.Button} onPress={() => navigate('Form')}>
