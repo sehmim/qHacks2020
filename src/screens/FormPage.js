6350c017ab2beed8195e93ff76c44e3b21a8b7af
sehmim2 <sehmim@Sehmims-MacBook-Pro.local>
2020-02-01 00:03:41 -0500

    adding form

--- src/screens/FormPage.js
+++ src/screens/FormPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
+import Constants from 'expo-constants';
+
 
 const sendData = (navigate) => {
     navigate("Results")
@@ -9,11 +11,31 @@ const sendData = (navigate) => {
 const FormPage = ({ navigation }) => {
     const { navigate } = navigation
     const [isSubmit, setIsSubmit] = React.useState(true)
+    const [name, setName] = React.useState('')
+    const [car, setCar] = React.useState('')
+
+
 
     return (
         <View style={styles.container}>
-            <View style={{ flex: 5 }}>
-                <Text style={{ color: 'white' }} ></Text>
+            <View style={styles.statusBar} />
+
+            <View style={{ padding: 10 }}>
+                <Text style={{ color: 'white', fontSize: 40, textAlign: 'center' }} >Info</Text>
+                <Text style={{ color: 'white' }}>Full Name: </Text>
+                <TextInput
+                    style={{ height: 30, color: 'white', fontSize: 20 }}
+                    placeholder="John Doe"
+                    onChangeText={(text) => setName(text)}
+                    value={name}
+                />
+                <Text style={{ color: 'white' }}>Car Type: </Text>
+                <TextInput
+                    style={{ height: 30, color: 'white', fontSize: 20 }}
+                    placeholder="Honda Civic"
+                    onChangeText={(text) => setCar(text)}
+                    value={car}
+                />
             </View>
             <TouchableOpacity
                 title="Submit"
@@ -29,6 +51,8 @@ const styles = StyleSheet.create({
         flex: 6,
         backgroundColor: 'rgb(14,198,221)',
     },
+    statusBar: {
+        height: Constants.statusBarHeight,
+    },
 });
 export default FormPage
-
