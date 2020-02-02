a43c790f96d9af56ee90c1fc609c9d58cae4de3c
AndrewHocking <andrew.hocking@hotmail.ca>
2020-02-02 00:23:58 -0500

    More flask

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+import pandas as pd
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
@@ -12,35 +15,94 @@ def hello():
 
 
 def predict(data):
-    X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-    nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
-    distances, indices = nbrs.kneighbors(X)
-    for n in indices:
-        print(n)
-    for n in distances:
-        print(n)
-    return data
+    # do ml and shit
+    result = {
+        "1": {
+            "name": "TD BANK",
+            "confidence": "93",
+            "quote": "436.25"
+        },
+        "2": {
+            "name": "AVIVA",
+            "confidence": "75",
+            "quote": "436.25"
+        },
+        "3": {
+            "name": "SCOTIABANK",
+            "confidence": "84",
+            "quote": "436.25"
+        },
+        "4": {
+            "name": "SUNLIFE",
+            "confidence": "65",
+            "quote": "436.25"
+        },
+        "5": {
+            "name": "INTACT",
+            "confidence": "83",
+            "quote": "436.25"
+        }
+    }
+    return result
+
+
+# a = [np.ones(5) *2]
+# a
+
+# df = pd.DataFrame(a)
+# df
+
+
 
 
 @app.route('/postjson', methods=['POST'])
 def postJsonHandler():
     print(request.is_json)
     content = request.get_json(force=True)
-    gender = content['gender']
+    gender = content["gender"]
+    # if (content['gender'][0] == 'm'):
+    #     gender = '1'
+    # else:
+    #     gender = '0'
     age = content['age']
-    city = content['city']
+    city = content['city'].lower()
     year = content['year']
     make = content['make']
     model = content['model']
     dist = content['dist']
     new_used = content['new_used']
-    arr = np.array([gender, age, city, year, make, model, dist, new_used])
+    # arr = np.array([gender, age, city, year, make, model, dist, new_used])
+
+
+    # city names
+    canada_cities = pd.read_csv('list-of-cities-in-canada-csv.csv', names=['city'])
+    l = list(pd.get_dummies(canada_cities, drop_first=True).columns)
+    l.sort()
+    k = {}
+    for i in enumerate(l):
+        k[i[1].split('_')[1].lower()] = i[0]
+    zero_list = list(np.zeros(len(k)))
+
+    for c in k:
+        if (city == c):
+            zero_list[k[c]-1] = 1
+    
+    
+    print(zero_list)
+    print(len(zero_list))
+
+
     # string = predict(arr)
-    string = "You are a " + age + " year old " + gender + " who drives a " + new_used + " " + \
-        year + " " + make + " " + model + " for " + \
-        dist + " km every year in " + city + "."
+    # string = "You are a " + age + " year old " + content['gender'] + " who drives a " + new_used + " " + \
+    #     year + " " + make + " " + model + " for " + \
+    #     dist + " km every year in " + city + "."
     print(jsonify(content))
-    return string
+
+    # name - TD BANK, AVIVA, SCOTIABANK, SUNLIFE, INTACT
+    # confidence - #
+    # quote - #
+
+    return jsonify(predict(content))
 
 
 app.run(host='0.0.0.0', port=8090)
